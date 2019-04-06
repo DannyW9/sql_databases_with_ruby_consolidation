ce31999d1081701ed8d4490b8e8fd53958d354c3
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-06 22:35:08 +0100

    adds method for deleting all and individual ticket entries

--- models/ticket.rb
+++ models/ticket.rb
@@ -24,6 +24,17 @@ class Ticket
     return results.map{ |ticket| Ticket.new(ticket) }
   end
 
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 
