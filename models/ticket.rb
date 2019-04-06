f6b3435e545577d578db2bce14f7f1f2f5de7768
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-06 22:31:42 +0100

    adds method for displaying all stored tickets

--- models/ticket.rb
+++ models/ticket.rb
@@ -18,6 +18,12 @@ class Ticket
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
   end
 
+  def self.all()
+    sql = "SELECT * FROM tickets"
+    results = SqlRunner.run(sql)
+    return results.map{ |ticket| Ticket.new(ticket) }
+  end
+
 
 
 
