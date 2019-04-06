5dfb5ddf5e531c4b9d17d7f85f0d6767cde2e590
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-06 22:29:24 +0100

    adds method for saving tickets to database

--- console.rb
+++ console.rb
@@ -50,27 +50,35 @@ ticket1 = Ticket.new({
   'film_id' => "#{film1.id}"
   })
 
+ticket1.save()
+
 ticket2 = Ticket.new({
   'customer_id' => "#{customer2.id}",
   'film_id' => "#{film1.id}"
   })
+
+ticket2.save()
 
 ticket3 = Ticket.new({
   'customer_id' => "#{customer3.id}",
   'film_id' => "#{film1.id}"
   })
 
+ticket3.save()
+
 ticket4 = Ticket.new({
   'customer_id' => "#{customer1.id}",
   'film_id' => "#{film2.id}"
   })
+
+ticket4.save()
 
 ticket5 = Ticket.new({
   'customer_id' => "#{customer3.id}",
   'film_id' => "#{film2.id}"
   })
 
-
+ticket5.save()
 
 
 binding.pry
--- models/ticket.rb
+++ models/ticket.rb
@@ -10,6 +10,13 @@ class Ticket
     @film_id = details['film_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO tickets (customer_id, film_id)
+            VALUES ($1, $2)
+            RETURNING id"
+    values = [@customer_id, @film_id]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
 
 
