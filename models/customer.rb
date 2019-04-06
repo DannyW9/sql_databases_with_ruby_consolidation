2332c885f95e83e6be040ad3597774322f439911
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-06 22:50:28 +0100

    adds method for displaying which films a customer has booked to see

--- models/customer.rb
+++ models/customer.rb
@@ -43,5 +43,14 @@ class Customer
     values = [@name, @funds, @id]
     SqlRunner.run(sql, values)
   end
-  
+
+  def films()
+    sql = "SELECT films.*  FROM films
+    INNER JOIN tickets ON films.id = tickets.film_id
+    WHERE tickets.customer_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    return results.map{ |film| Film.new(film) }
+  end
+
 end
