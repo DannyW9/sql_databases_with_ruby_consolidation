65d89eb87b9e4dbc37f89e370534c4551b1fde7a
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-06 20:54:44 +0100

    adds method for saving films to database

--- console.rb
+++ console.rb
@@ -6,17 +6,21 @@ film1 = Film.new({
   'price' => '10'
   })
 
+film1.save()
+
 film2 = Film.new({
   'title' => 'The Lion King',
   'price' => '15'
   })
+
+film2.save()
 
 film3 = Film.new({
   'title' => 'Avengers: Endgame',
   'price' => '12'
   })
 
-
+film3.save()
 
 
 binding.pry
--- models/film.rb
+++ models/film.rb
@@ -10,6 +10,12 @@ class Film
     @price = details['price'].to_i
   end
 
-
+  def save()
+    sql = "INSERT INTO films (title, price)
+            VALUES ($1, $2)
+            RETURNING id"
+    values = [@title, @price]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
 
 end
